dcbf2c698e2e27665d04afd1ff697f20e091be8f
JoanSebastian <Joan>
2022-03-25 15:39:27 +0100

    Viernes

--- src/app/components/apps-list/apps-list.component.ts
+++ src/app/components/apps-list/apps-list.component.ts
@@ -10,26 +10,35 @@ import { App } from '../../models/apps.model';
 export class AppListComponent {
 
   /*Propiedad del componente para definir el orden de las tareas.*/
-  @Input() appsInOrder: App[] = [];
+  @Input() appInOrder: App[] = [];
 
   /*Comprueba si está en estado de carga */
   @Input() loading:boolean
   
-  /* Evento para cambiar la tarea a anclada */
-  // tslint:disable-next-line: no-output-on-prefix
-  @Output() onPinTask = new EventEmitter<Event>();
+  // /* Evento para cambiar la tarea a anclada */
+  // // tslint:disable-next-line: no-output-on-prefix
+  // @Output() onPinTask = new EventEmitter<Event>();
   
-  /* Evento para cambiar la tarea a archivada */
-  // tslint:disable-next-line: no-output-on-prefix
-  @Output() onArchiveTask = new EventEmitter<Event>();
-  @Output() calendar = new EventEmitter<Event>();
+  // /* Evento para cambiar la tarea a archivada */
+  // // tslint:disable-next-line: no-output-on-prefix
+  // @Output() onArchiveTask = new EventEmitter<Event>();
+
+  //Calendar
+  @Output() clickCalendar = new EventEmitter<App>();
+
+  //Switch
+  @Output() clickSwitch = new EventEmitter<App>();
   
   @Input()
   set apps(arr: App[]) {
-    this.appsInOrder = [
+    this.appInOrder = [
     ...arr.filter(t => t.state === 'TASK_PINNED'),
     ...arr.filter(t => t.state !== 'TASK_PINNED'),
     ];
+  }
+
+  toggle(event,app){
+    console.log(event.checked,app);
   }
 
   showElement(){
